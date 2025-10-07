a261330db26613864f7fdbb115842fa603db5f82
ELIT5 <hhh4809746@gmail.com>
2025-10-07 19:42:55 +0200

    Laba3 added

--- Laba2/Laba2/Laba2.cpp
+++ Laba2/Laba2/Laba2.cpp
@@ -6,10 +6,18 @@ int main()
 {
     SetConsoleOutputCP(1251); // Вывод на консоль в кодировке 1251.
     std::cout << "1." << '\n';
-    int A,i;
+    int i;
+    short A;
+    bool bit_i;
     std::cin >> A >> i;
-    if (i > 0) {
-        if ((A & (1 << i - 1)) == (A & (0 << i - 1))) {
+    bit_i = A == (A | (1 << i));
+    if (i >= 0 && i < 15) {
+        if (bit_i != 0) {
+            A = A & ~(~A | 1 << (i));
+            std::bitset<16> A1 = A;
+            std::cout << A1.to_string() << '\n';
+        }
+        else {
             int x1, x2;
             std::cin >> x1 >> x2;
             if ((x1 > 0 && x2 > 0 && x1 > x2) || (x1 < 0 && x2 < 0 && x1 < x2) || (x1 > 0 && x2 < 0 && x1 > -x2) || (x1 < 0 && x2 > 0 && -x1 > x2)) {
@@ -19,16 +27,11 @@ int main()
                 std::cout << x2 << x2 << '\n';
             }
         }
-        else {
-            A = A & ~(~A | 1 << (i-1));
-            std::bitset<8> A1 = A;
-            std::cout << A1.to_string() << '\n';
-        }
     }
     std::cout << "2." << '\n';
     int N,M;
     std::cin >> N >> M;
-    if (0 < N < 10 && 0 < M < 10) {
+    if (0 < N && N < 10 && 0 < M && M < 10) {
         switch (10 * N + M)
         {
         case 11: std::cout << "Барабанные палочки"; break;
---
+++ Laba3/Laba3/Laba3.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    int n, first,a,sum = 0,min = 10000000,n_min,max_digit = 0,min_digit = 9;
+    std::cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        std::cin >> a;
+        if (i == 1) {
+            first = a;
+            continue;
+        }
+        if (a > first) {
+            sum += a;
+            if (a < min) {
+                min = a;
+                n_min = i;
+            }
+        }
+    }
+    std::cout << "Сумма всех, больше первого " << sum << " Наименьшее из таких " << min << " Его номер в последовательности " << n_min << std::endl;
+    while (sum > 0) {
+        if (max_digit < sum % 10) {
+            max_digit = sum % 10;
+        }
+        else if (min_digit > sum % 10) {
+            min_digit = sum % 10;
+        }
+        sum /= 10;
+    }
+
+    std::cout << "Сумма наименьшей и наибольшей цифр " << max_digit + min_digit;
+}
