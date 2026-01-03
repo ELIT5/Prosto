f56fa4d16a9e8e728350189c35d0b175b551bfc4
ELIT5 <hhh4809746@gmail.com>
2026-01-03 17:23:49 +0300

    Add exam

---
+++ Exam/Exam/Exam.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <array>
+
+std::array<short, 10> func(unsigned short n) {
+    short digit;
+	std::array<short, 10> digits = { 0 };
+	while (n != 0)
+	{
+		digit = n % 10;
+		n /= 10;
+		digits[digit]++;
+	}
+	return digits;
+}	
+
+int main()
+{
+	unsigned short n;
+	std::array<short, 10> digits = func(n);
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << digits[i] << std::endl;
+	}
+}
